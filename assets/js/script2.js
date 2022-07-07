07feb5df14a0ed25def97ddf6848bd466fa7ce71
Eliot Cleveland <eclevela@richmond.edu>
2022-07-07 10:08:01 -0400

    api call succesfully selects random choice from category input

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -2,31 +2,33 @@
 var apiList = [];
 
 // Function to call API API
-// var apisquared = function (choice) {
-//   fetch("https://api.publicapis.org/entries").then(function (response) {
-//     if (response.ok) {
-//       response.json().then(function (data) {
-//         for (var i = 0; i < data.entries.length; i++) {
-//             if (data.entries[i].Category === choice) {
-//                 const apiEntry = {Name: data.entries[i].API, Description: data.entries[i].Description, Link: data.entries[i].Link};
-//                 apiList.push(apiEntry);
-//             }
-//         }
-//       });
-//       console.log(apiList)
-//     }
-//   });
-// };
+var apisquared = function (choice) {
+  fetch("https://api.publicapis.org/entries?category=" + choice).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        for (var i = 0; i < data.entries.length; i++) {
+            if (data.entries[i].Category === choice) {
+                var apiEntry = {Name: data.entries[i].API, Description: data.entries[i].Description, Link: data.entries[i].Link};
+                apiList.push(apiEntry);
+            }
+        }
+      });
+      console.log(apiList)
+      var apiChoice = apiList[Math.floor(Math.random() * apiList.length)];
+      console.log(apiChoice);
+    }
+  });
+};
 
 // apisquared("Animals");
 
 
-// Function to call random user
-var randomUser = function(number,) {
+// // Function to call random user
+// var randomUser = function(number,) {
 
-    var apiUrl = "https://randomuser.me/api/1.4/" + number
+//     var apiUrl = "https://randomuser.me/api/1.4/" + number
 
-}
+// }
 // Function to call Random text
 
 // Function deal with output
