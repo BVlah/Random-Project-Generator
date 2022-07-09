f7e7ed6fd0debd87c21e59f8793c2930529f4090
Eliot Cleveland <eclevela@richmond.edu>
2022-07-09 10:45:59 -0400

    modal and api call/display connected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,40 @@
-
 var projects = [];
 
 // Open Modal
-$("#start-project").on("click", function() {
-    $('#project-modal')[0].classList.add('is-active');
+$("#start-project").on("click", function () {
+  $("#project-modal")[0].classList.add("is-active");
 });
 
+// Submit Button in Modal Clicked
+$("#project-form-modal").click(function (event) {
+  event.preventDefault();
+  // get form values
+  var projectTeammates = $("#modalTeammates").val();
+  var projectCategory = $("#modalCategory").val();
+  var projectBackstory = $("#modalBackstory").val();
 
- // Submit Button in Modal Clicked
- $("#project-form-modal").click(function(event) {
-    event.preventDefault()
-    // get form values
-    var projectTeammates = $("#modalTeammates").val();
-    var projectCategory = $("#modalCategory").val();
-    var projectBackstory = $("#modalBackstory").val();
+  if (projectCategory != "Select One" && projectBackstory && projectTeammates) {
+    // close modal
+    $("#project-modal")[0].classList.remove("is-active");
 
-    if (projectCategory != "Select One" && projectBackstory && projectTeammates) {
-        
-      // close modal
-      $("#project-modal")[0].classList.remove('is-active');
+    // save in projects array
+    // projects.push({
+    //   teammates: projectTeammates,
+    //   category: projectCategory,
+    //   backstory: projectBackstory
+    // });
+    randomUserCall(projectTeammates);
+    apiSquaredCall(projectCategory);
 
-      // save in projects array
-      projects.push({
-        teammates: projectTeammates,
-        category: projectCategory,
-        backstory: projectBackstory
-      });
-      console.log(projects);
-    }
-    else alert("Please fill out all info!");
- });
+    console.log(projectBackstory);
+  } else alert("Please fill out all info!");
+});
 
 // Add additional click events to close modal
-$('.modal-background, .modal-close').click(function(event) {
-    event.preventDefault();
+$(".modal-background, .modal-close").click(function (event) {
+  event.preventDefault();
 
-    $("#project-modal")[0].classList.remove('is-active');
+  $("#project-modal")[0].classList.remove("is-active");
 });
 
 var sampleRandomUsers = [
@@ -119,11 +118,12 @@ var displayApiChoices = function (apiChoices) {
   apiEl.innerHTML = "";
   var titleEl = document.createElement("p");
   titleEl.classList = "title";
-  titleEl.textContent = "These APIs are perfect for your " + choice + " project!";
+  titleEl.textContent =
+    "These APIs are perfect for your " + choice + " project!";
   apiEl.appendChild(titleEl);
   for (let i = 0; i < apiChoices.length; i++) {
     var divEl = document.createElement("article");
-    divEl.classList = "m-"
+    divEl.classList = "m-";
     var pHeadEl = document.createElement("p");
     var pSubEl = document.createElement("p");
     var aEl = document.createElement("a");
@@ -139,7 +139,6 @@ var displayApiChoices = function (apiChoices) {
     apiEl.appendChild(divEl);
   }
 };
-
 
 // Function to call random user
 var randomUserCall = function (userCount) {
@@ -170,31 +169,32 @@ var displayUserChoices = function (randUserList) {
   titleEl.textContent = "Meet your team!";
   userEl.appendChild(titleEl);
   for (let i = 0; i < randUserList.length; i++) {
-      var mediaEl = document.createElement("div");
-      mediaEl.classList = "media is-justify-content-center is-flex-direction-row-reverse";
-      userEl.appendChild(mediaEl);
-      var mediaLeftEl = document.createElement("div");
-      mediaLeftEl.classList = "media-left";
-      mediaEl.appendChild(mediaLeftEl);
-      var figureEl = document.createElement("figure");
-      figureEl.classList = "image is-64x64";
-      mediaLeftEl.appendChild(figureEl);
-      var imgEl = document.createElement("img");
-      imgEl.classList = "is-rounded";
-      imgEl.setAttribute("src", randUserList[i].Image);
-      imgEl.setAttribute("alt", "profile image of" + randUserList[i].Name);
-      figureEl.appendChild(imgEl);
-      var mediaContentEl = document.createElement("div");
-      mediaContentEl.classList = "media-content media-left";
-      mediaEl.appendChild(mediaContentEl);
-      var nameEl = document.createElement("p");
-      nameEl.classList = "title is-5";
-      nameEl.textContent = randUserList[i].Name;
-      mediaContentEl.appendChild(nameEl);
-      var usernameEl = document.createElement("p");
-      usernameEl.classList = "subtitle is-6";
-      usernameEl.textContent = "@" + randUserList[i].Username;
-      mediaContentEl.appendChild(usernameEl);
+    var mediaEl = document.createElement("div");
+    mediaEl.classList =
+      "media is-justify-content-center is-flex-direction-row-reverse";
+    userEl.appendChild(mediaEl);
+    var mediaLeftEl = document.createElement("div");
+    mediaLeftEl.classList = "media-left";
+    mediaEl.appendChild(mediaLeftEl);
+    var figureEl = document.createElement("figure");
+    figureEl.classList = "image is-64x64";
+    mediaLeftEl.appendChild(figureEl);
+    var imgEl = document.createElement("img");
+    imgEl.classList = "is-rounded";
+    imgEl.setAttribute("src", randUserList[i].Image);
+    imgEl.setAttribute("alt", "profile image of" + randUserList[i].Name);
+    figureEl.appendChild(imgEl);
+    var mediaContentEl = document.createElement("div");
+    mediaContentEl.classList = "media-content media-left";
+    mediaEl.appendChild(mediaContentEl);
+    var nameEl = document.createElement("p");
+    nameEl.classList = "title is-5";
+    nameEl.textContent = randUserList[i].Name;
+    mediaContentEl.appendChild(nameEl);
+    var usernameEl = document.createElement("p");
+    usernameEl.classList = "subtitle is-6";
+    usernameEl.textContent = "@" + randUserList[i].Username;
+    mediaContentEl.appendChild(usernameEl);
   }
 };
 
@@ -203,6 +203,5 @@ var displayUserChoices = function (randUserList) {
 // apiSquaredCall("Business");
 
 // Display Samples - For Testing Purposes
-displayUserChoices(sampleRandomUsers);
-displayApiChoices(sampleApiChoices);
-
+// displayUserChoices(sampleRandomUsers);
+// displayApiChoices(sampleApiChoices);
